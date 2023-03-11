64b1b88d9f8c2b059c74f495773e227f62250ef3
Bianca M <matthews.biancam@gmail.com>
2023-03-10 20:24:29 -0600

    Added help, get, and set

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ import markov
 
 filename = './texts/twain.txt'
 chains = markov.make_chains(markov.open_and_read_file([filename]))
+#def new_chains(filename):
+    #global
+    #chains = markov.make_chains(markov.open_and_read_file([filename]))
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -17,12 +20,33 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    global chains
     if message.author == client.user:
         return
 
-    text = markov.make_text(chains)
-
-    if message.content.startswith('$hello'):
-        await message.channel.send(text)
+    if message.content.startswith('$mb'):
+        tokens = message.content.split()
+        messageToSend = ''
+        if tokens[1] == 'hello':
+            messageToSend = markov.make_text(chains)
+        elif tokens[1] == 'help':
+            messageToSend = f"'$mb hello' to generate some text from a certain source\n'$mb get' to see available sources\n'$mb set <filename>' to set a source file"
+        elif tokens[1] == 'get':
+            for filename in os.listdir('./texts'):
+                messageToSend += f'{filename}     '
+        elif tokens[1] == 'set':
+            #try:
+            if os.path.isfile('./texts/' + tokens[2]):
+                chains = markov.make_chains(markov.open_and_read_file([f'./texts/{tokens[2]}']))
+                #new_chains(f'./texts/{tokens[2]}')
+                messageToSend = f"Thanks! Source text is now {tokens[2]}"
+            else:
+                messageToSend = f"Sorry, {tokens[2]} not found. Please call '$mb get' to see available texts."
+            # except:
+                # messageToSend = f"Proper syntax: '$mb set <filename here>' /n'$mb get' to see available texts"
+        else:
+           messageToSend = "Sorry, I don't understand that" 
+        
+        await message.channel.send(messageToSend)
 
 client.run(os.environ['DISCORD_TOKEN'])
--- markov.py
+++ markov.py
@@ -55,16 +55,16 @@ def make_text(chains):
     return ' '.join(words)
 
 
-# Get the filenames from the user through a command line prompt, ex:
-# python markov.py green-eggs.txt shakespeare.txt
-# filenames = sys.argv[1:]
+if __name__=='main':
+    # Get the filenames from the user through a command line prompt, ex:
+    # python markov.py green-eggs.txt shakespeare.txt
+    # filenames = sys.argv[1:]
+    if not filenames:
+        print("Please provide a filename on the command line! Ex. python3 markov.py green-eggs.txt")
+        exit(1)
 
-# if not filenames:
-    # print("Please provide a filename on the command line! Ex. python3 markov.py green-eggs.txt")
-    # exit(1)
+    # Open the files and turn them into one long string
+    text = open_and_read_file(filenames)
 
-# # Open the files and turn them into one long string
-# text = open_and_read_file(filenames)
-
-# # Get a Markov chain
-# chains = make_chains(text)
+    # Get a Markov chain
+    chains = make_chains(text)
